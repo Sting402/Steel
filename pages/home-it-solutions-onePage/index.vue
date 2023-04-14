ac5b0c31f3e4b4ee45a17d9fc5d8c9e516535aee
Sting402 <steven19930326@gmail.com>
2023-04-15 05:03:14 +0800

    人才招募

--- pages/home-it-solutions-onePage/index.vue
+++ pages/home-it-solutions-onePage/index.vue
@@ -1,48 +1,8 @@
 <template>
   <div>
-    <Navbar />
-    <Header />
-    <main>
-      <Services />
-      <ChooseUs />
-      <Reviews />
-      <Pricing />
-      <Blog />
-      <Contact />
-    </main>
-    <Footer />
+    人才招募
   </div>
 </template>
 
 <script>
-import Navbar from '../../components/Navbars/ITNav/OnePageNav';
-import Header from '../../components/IT/Header';
-import Services from '../../components/IT/Services';
-import ChooseUs from '../../components/IT/ChooseUs';
-import Reviews from '../../components/IT/Reviews';
-import Pricing from '../../components/IT/Pricing';
-import Blog from '../../components/IT/Blog';
-import Contact from '../../components/IT/Contact';
-import Footer from '../../components/IT/Footer';
-
-export default {
-  name: 'HomeITSolutionsOnePage',
-  layout: "Main",
-  components: {
-    Navbar,
-    Header,
-    Services,
-    ChooseUs,
-    Reviews,
-    Pricing,
-    Blog,
-    Contact,
-    Footer
-  },
-  head() {
-    return {
-      titleTemplate: "%s - IT Solutions One Page",
-    }
-  }
-}
 </script>
