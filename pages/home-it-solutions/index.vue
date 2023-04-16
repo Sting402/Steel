515585be7c3e8c6c18f12aea9d65be8bf639146d
Sting402 <steven19930326@gmail.com>
2023-04-16 22:32:41 +0800

    加入地圖，未加標點

--- pages/home-it-solutions/index.vue
+++ pages/home-it-solutions/index.vue
@@ -1,8 +1,50 @@
 <template>
   <div>
-    公司地圖
+    <PreLoader />
+    <Navbar ref="navbarRef" />
+    
+    <main class="contact-page style-5">
+      <h1>公司地圖</h1>
+      <Map />
+      
+    </main>
   </div>
 </template>
 
 <script>
+import navbarScrollEffect from "../../common/navbarScrollEffect";
+import PreLoader from "../../components/PreLoader/index.vue";
+import ScrollToTop from "../../components/ScrollToTop/index.vue";
+import Navbar from '../../components/Navbars/PreviewNav';
+import Map from '../../components/Contact/Map';
+
+
+export default {
+  components: {
+    PreLoader,
+    ScrollToTop,
+    Navbar,
+    Map
+    
+  },
+  head() {
+    return {
+      link: [
+        { rel: 'stylesheet', href:'/assets/css/lib/bootstrap-icons.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/swiper.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/bootstrap.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/all.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/animate.css' },
+        { rel: 'stylesheet', href:'/assets/css/style.css' },
+        { rel: 'stylesheet', href:'/landing-preview/css/preview-style.css' },
+      ],
+      script: [
+        { src: "/landing-preview/js/parallax.min.js" }
+      ]
+    }
+  },
+  mounted() {
+    navbarScrollEffect(this.$refs.navbarRef.$el);
+  }
+}
 </script>
