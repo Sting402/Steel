ed19857d3b7f0cfa57b84fd8cb4518310f066451
Sting402 <steven19930326@gmail.com>
2023-04-16 22:31:14 +0800

    聯絡我們頁面雛形

--- pages/home-help-desk/index.vue
+++ pages/home-help-desk/index.vue
@@ -1,8 +1,49 @@
 <template>
   <div>
-    聯絡我們
+    
+    <Navbar ref="navbarRef" />
+    <main class="contact-page style-5">
+      <Form styleType="5" />
+      <Map />
+    </main>
+    <ScrollToTop />
+    <Footer :noWave="true" />
   </div>
 </template>
 
 <script>
+import navbarScrollEffect from "../../common/navbarScrollEffect";
+import Form from "../../components/Contact/Form";
+import Map from "../../components/Contact/Map";
+import Navbar from "../../components/Navbars/PreviewNav";
+import Footer from "../../components/Saas/Footer";
+import ScrollToTop from "../../components/ScrollToTop/index.vue";
+
+export default {
+  name: "PageContact5",
+  layout: "Main",
+  components: {
+    ScrollToTop,
+    Navbar,
+    Form,
+    Map,
+    Footer,
+  },
+  head() {
+    return {
+      link: [
+        { rel: "stylesheet", href: "/assets/css/lib/bootstrap-icons.css" },
+        { rel: "stylesheet", href: "/assets/css/lib/swiper.min.css" },
+        { rel: "stylesheet", href: "/assets/css/lib/bootstrap.min.css" },
+        { rel: "stylesheet", href: "/assets/css/lib/all.min.css" },
+        { rel: "stylesheet", href: "/assets/css/lib/animate.css" },
+        { rel: "stylesheet", href: "/assets/css/style.css" },
+        { rel: "stylesheet", href: "/landing-preview/css/preview-style.css" },
+      ],
+    };
+  },
+  mounted() {
+    navbarScrollEffect(this.$refs.navbarRef.$el);
+  },
+};
 </script>
