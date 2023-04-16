d328c3bcccdb8383388257c7b4ec437594a5b368
Sting402 <steven19930326@gmail.com>
2023-04-16 22:37:29 +0800

    每頁加入正確Nav

--- pages/home-app-landing-onePage/index.vue
+++ pages/home-app-landing-onePage/index.vue
@@ -1,9 +1,50 @@
 <template>
   <div>
-    公司沿革
+    <PreLoader />
+    <Navbar ref="navbarRef" />
+    
+    <main class="contact-page style-5">
+      <h1>公司沿革</h1>
+      
+      
+    </main>
   </div>
 </template>
 
 <script>
+import navbarScrollEffect from "../../common/navbarScrollEffect";
+import PreLoader from "../../components/PreLoader/index.vue";
+import ScrollToTop from "../../components/ScrollToTop/index.vue";
+import Navbar from '../../components/Navbars/PreviewNav';
 
+
+
+export default {
+  components: {
+    PreLoader,
+    ScrollToTop,
+    Navbar,
+    
+    
+  },
+  head() {
+    return {
+      link: [
+        { rel: 'stylesheet', href:'/assets/css/lib/bootstrap-icons.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/swiper.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/bootstrap.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/all.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/animate.css' },
+        { rel: 'stylesheet', href:'/assets/css/style.css' },
+        { rel: 'stylesheet', href:'/landing-preview/css/preview-style.css' },
+      ],
+      script: [
+        { src: "/landing-preview/js/parallax.min.js" }
+      ]
+    }
+  },
+  mounted() {
+    navbarScrollEffect(this.$refs.navbarRef.$el);
+  }
+}
 </script>
--- pages/home-cyber-security/index.vue
+++ pages/home-cyber-security/index.vue
@@ -1,8 +1,50 @@
 <template>
   <div>
-    作業流程
+    <PreLoader />
+    <Navbar ref="navbarRef" />
+    
+    <main class="contact-page style-5">
+      <h1>作業流程</h1>
+      
+      
+    </main>
   </div>
 </template>
 
 <script>
+import navbarScrollEffect from "../../common/navbarScrollEffect";
+import PreLoader from "../../components/PreLoader/index.vue";
+import ScrollToTop from "../../components/ScrollToTop/index.vue";
+import Navbar from '../../components/Navbars/PreviewNav';
+
+
+
+export default {
+  components: {
+    PreLoader,
+    ScrollToTop,
+    Navbar,
+    
+    
+  },
+  head() {
+    return {
+      link: [
+        { rel: 'stylesheet', href:'/assets/css/lib/bootstrap-icons.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/swiper.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/bootstrap.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/all.min.css' },
+        { rel: 'stylesheet', href:'/assets/css/lib/animate.css' },
+        { rel: 'stylesheet', href:'/assets/css/style.css' },
+        { rel: 'stylesheet', href:'/landing-preview/css/preview-style.css' },
+      ],
+      script: [
+        { src: "/landing-preview/js/parallax.min.js" }
+      ]
+    }
+  },
+  mounted() {
+    navbarScrollEffect(this.$refs.navbarRef.$el);
+  }
+}
 </script>
